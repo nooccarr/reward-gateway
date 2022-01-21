2954f8a6c26a60068435d84d64d812fcc628a4c5
Soohyun Lim <d2axasap@gmail.com>
2022-01-21 17:33:54 -0500

    Create client folder. Create dist and src folder inside. Add index.html and add div with id property named app and script property named bundle.js for running react. Create index.jsx to src folder and then add reactDOM and have it import App component.

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app'
+
+let mountNode = document.getElementById('app');
+ReactDOM.render(<App/>, mountNode);
